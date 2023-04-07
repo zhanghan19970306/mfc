f1288e07ccdd131e5b79bb2cf11d1ee98b9c74e5
张涵 <zhanghan@qmai.cn>
2023-04-07 14:02:44 +0800

    🚀 perf: 优化脚本速度

--- scripts/gitHooks/commit-msg.js
+++ scripts/gitHooks/commit-msg.js
@@ -1,9 +1,5 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import { resolve } from 'node:path'
-import { default as chalk } from 'chalk'
-// import { errorBoxen } from '../boxen'
-
-const DOC_URL = 'https://framework.pages.zmcms.cn/spec/git/commitLint.html'
 
 const TYPE_MAP = new Map([
   ['feat', { emoji: '✨', title: 'feat', description: '新的特性' }],
@@ -48,9 +44,17 @@ if (/Merge.+branch \'.+\'/.test(msg)) {
 
 // core lint
 if (!commitRE.test(msg)) {
-  console.log(`本次commit message不符合规范！\n查看更多:${chalk.blue(DOC_URL)}`)
-  // errorBoxen(`本次commit message不符合规范！\n查看更多:${chalk.blue(DOC_URL)}`)
-  process.exit(0)
+  console.log(
+    [
+      '\x1B[31m\x1B[39m\n' +
+        '\x1B[31m╔ error ════════════════════════════════════════════════════════════════╗\x1B[39m\n' +
+        '\x1B[31m║\x1B[39m 本次commit message不符合规范！                                        \x1B[31m║\x1B[39m\n' +
+        '\x1B[31m║\x1B[39m 查看更多:\x1B[34mhttps://qimaitech.feishu.cn/wiki/wikcnSOiZCoGVJ2AQ31c48MCaIe\x1B[39m \x1B[31m║\x1B[39m\n' +
+        '\x1B[31m╚═══════════════════════════════════════════════════════════════════════╝\x1B[39m\n' +
+        '\x1B[31m\x1B[39m',
+    ].toString()
+  )
+  process.exit(1)
 }
 
 // add emoji in before
