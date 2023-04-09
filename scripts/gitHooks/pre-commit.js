8c9b1bd427612de2e1a9ec6a6a2063f796e0a215
张涵 <zhanghan@qmai.cn>
2023-04-09 14:16:52 +0800

    🔋 wip: 继续尝试pre-commit hook

--- scripts/gitHooks/pre-commit.js
+++ scripts/gitHooks/pre-commit.js
@@ -1,13 +1,24 @@
 import { $ } from 'execa'
-import { intro, outro, confirm, select, spinner, isCancel, cancel, text } from '@clack/prompts'
+import { intro, outro, spinner, isCancel, cancel, text, log } from '@clack/prompts'
 
-console.log(123)
-
-intro('即将开始进行代码检测')
+intro('开始进行代码检测')
 
 const s = spinner()
-s.start('检测目录/文件的正确命名')
-await $`ls-lint --warn`
-s.stop('检测目录/文件的正确命名')
 
-process.exit(1)
+{
+  s.start('开始进行ls-lint检测')
+  const { stdout } = await $`ls-lint --warn`
+  s.stop('ls-lint检测完毕')
+  if (stdout.trim()) {
+    log.error(stdout)
+    process.exit(1)
+  }
+}
+
+s.start('开始进行eslint检测')
+s.stop('eslint检测完毕')
+
+s.start('开始进行stylelint检测')
+s.stop('stylelint检测完毕')
+
+outro('代码检测完成')
