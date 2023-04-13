56db62a7713a0fdbaed41064fe3b1051002cd095
张涵 <zhanghan@qmai.cn>
2023-04-13 23:02:34 +0800

    🔋 wip: a

--- scripts/gitHooks/post-checkout.js
+++ scripts/gitHooks/post-checkout.js
@@ -1,1 +1,5 @@
-console.log(3)
+import { $ } from 'execa'
+
+const { stdout: currentBranch } = await $`git rev-parse --abbrev-ref HEAD`
+
+console.log`post-checkout ==> 当前分支名称: ${currentBranch}`
--- scripts/gitHooks/prepare-commit-msg.js
+++ scripts/gitHooks/prepare-commit-msg.js
@@ -1,8 +1,5 @@
 import { existsSync, readFileSync } from 'node:fs'
 import { $ } from 'execa'
-import { log } from 'node:console'
-
-console.log('prepare-commit-msg')
 
 const isMerging = existsSync('.git/MERGE_MSG') && existsSync('.git/MERGE_HEAD')
 if (!isMerging) process.exit(0)
@@ -13,16 +10,34 @@ if (currentBranch.trim() !== 'master') process.exit(0)
 const mergeMsg = readFileSync('.git/MERGE_MSG', 'utf-8').trim()
 const hasConflicts = /\n# Conflicts:\n/.test(mergeMsg)
 if (hasConflicts) {
-  console.log('此次合并含有Conflicts解决后的merge节点, 不符合规范！')
+  console.log(
+    [
+      '\x1B[31m\x1B[39m\n' +
+        '\x1B[31m╔ error ════════════════════════════════════════════════════════════════════════════════════════════╗\x1B[39m\n' +
+        '\x1B[31m║\x1B[39m 此次合并含有Conflicts解决后的merge节点, 不符合规范！                                              \x1B[31m║\x1B[39m\n' +
+        '\x1B[31m║\x1B[39m 查看更多:\x1B[34mhttps://qimaitech.feishu.cn/wiki/wikcnSOiZCoGVJ2AQ31c48MCaIe#BmKOd2Q8eow08sxo7EvcacFBnaf\x1B[39m \x1B[31m║\x1B[39m\n' +
+        '\x1B[31m╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝\x1B[39m\n' +
+        '\x1B[31m\x1B[39m',
+    ].toString()
+  )
   process.exit(1)
 }
 
 const mergeHeadSha = readFileSync('.git/MERGE_HEAD', 'utf-8').trim()
 const { stdout: diffCountText } = await $`git rev-list --count master..${mergeHeadSha}`
 const diffCount = Number(diffCountText.trim())
-
-log(diffCountText)
 if (diffCount > 1) {
-  console.log(`此次合并含${diffCount}条Commit差异, 不符合规范！`)
+  console.log(
+    [
+      '\x1B[31m\x1B[39m\n' +
+        '\x1B[31m╔ error ════════════════════════════════════════════════════════════════════════════════════════════╗\x1B[39m\n' +
+        '\x1B[31m║\x1B[39m 此次合并含' +
+        diffCount +
+        '条Commit差异, 不符合规范！                                                             \x1B[31m║\x1B[39m\n' +
+        '\x1B[31m║\x1B[39m 查看更多:\x1B[34mhttps://qimaitech.feishu.cn/wiki/wikcnSOiZCoGVJ2AQ31c48MCaIe#BmKOd2Q8eow08sxo7EvcacFBnaf\x1B[39m \x1B[31m║\x1B[39m\n' +
+        '\x1B[31m╚═══════════════════════════════════════════════════════════════════════════════════════════════════╝\x1B[39m\n' +
+        '\x1B[31m\x1B[39m',
+    ].toString()
+  )
   process.exit(1)
 }
