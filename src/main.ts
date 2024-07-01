9897c872410c7953c279c261837a29d01ba3282c
张涵 <zhanghan@qmai.cn>
2024-07-01 22:21:24 +0800

    feat: 初始项目

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // 设置全局路由前缀
+  app.setGlobalPrefix('api');
+
+  // 监听端口
+  await app.listen(3000);
+}
+bootstrap();
